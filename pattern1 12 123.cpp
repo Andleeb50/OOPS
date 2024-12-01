dc6460b812bb8904d4b3fd9f19460473fb8995e2
Andleeb50 <andleebmuneer22@gmail.com>
2024-12-01 13:52:36 +0530

    pattern1 12 123.cpp

--- pattern1 12 123.cpp
+++ pattern1 12 123.cpp
@@ -4,20 +4,30 @@
   1  2  3  
 1  2  3   4
  */
+//for loop pattern
+/*    1
+    1  2
+  1  2  3  
+1  2  3   4
+ */
+
 #include <iostream>
 using namespace std;
 
-	int main() {
-	int n,j,i;
-	cout << "Enter the no of rows";
-	cin>>n;
-	if(i=0;i<=n;i++) {
-		
-		if(j=0;j<=n;j++) {
-		cout<<j<<" ";
-	}
-	cout<<endl;
-	}
-	}
-	return 0;
+int main() {
+    int n, j, i;
+    cout << "Enter the number of rows: ";
+    cin >> n;
+
+    // rows
+    for (i = 0; i < n; i++) {
+        // columns
+        for (j = 0; j <= i; j++) {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
 }
+
